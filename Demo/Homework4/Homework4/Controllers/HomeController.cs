4462d1b4eb2440b53730877148581c58aca547a5
Mason <mstokes13@wou.edu>
2018-10-21 19:40:20 -0700

    add files

--- Demo/Homework4/Homework4/Controllers/HomeController.cs
+++ Demo/Homework4/Homework4/Controllers/HomeController.cs
@@ -15,38 +15,54 @@ namespace Homework4.Controllers
             return View();
         }
 
-        public ActionResult Converter()
-        {
-            return View();
-        }
-
-        [HttpGet]
-        public ActionResult ColorChooser()
-        {
-            return View();
-        }
-
         [HttpGet]
         public ActionResult Mile()
         {
-            Debug.WriteLine(Request.QueryString["user_name"]);
-            Debug.WriteLine(Request.QueryString["user_email"]);
-            Debug.WriteLine(Request.QueryString["user_message"]);
+            double miles = Convert.ToDouble(Request.QueryString["miles"]);
+            string metric = Request.QueryString["metric"];
+            double output = 0;
+            Debug.WriteLine(miles);
+            Debug.WriteLine(metric);
+            ViewBag.result = false;
 
-            string name = Request.QueryString["user_name"];
-            if(name != null)
+            if(metric == "mm")
             {
-                string message = "Hello" + name + "! Welcome.";
-                ViewBag.message = message;
+                output = miles * 1609344;
+                ViewBag.result = true;
             }
-            
+            else if (metric == "cm")
+            {
+                output = miles * 160934.4;
+                ViewBag.result = true;
+            }
+            else if (metric == "m")
+            {
+                output = miles * 1609.344;
+                ViewBag.result = true;
+            }
+            else if (metric == "km")
+            {
+                output = miles * 1.609344;
+                ViewBag.result = true;
+            }
+
+            string message = miles + "miles is equal to " + output + " " + metric;
+
+            ViewBag.Message = message;
+
             return View();
         }
 
-        [HttpPost]
-        public ActionResult Mile2()
+        public ActionResult Miles()
         {
-            return View();
+            ///What to use for the color changer html page.
+            //@using (Html.BeginForm("Miles","Home",FormMethod.Post))
+            //{
+                //@Html.Label("Name","Name")
+                //@Html.TextBox("Name", null,htmlAttributes:new { @class="form-control", type="number"})
+            //}
+            return View();//@Html.Label("Name","Name")
         }
+        
     }
 }

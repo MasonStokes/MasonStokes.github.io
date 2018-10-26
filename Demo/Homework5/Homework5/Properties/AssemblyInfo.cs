93a5625b17618ddbfd0d633cffc18583d3ad3f6a
Mason <mstokes13@wou.edu>
2018-10-25 21:10:18 -0700

    rebuilt homework4

--- Demo/Homework5/Homework5/Properties/AssemblyInfo.cs
+++ Demo/Homework5/Homework5/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("fadbc531-070e-4fed-b31b-525740875560")]
+[assembly: Guid("526ae5a9-815d-458d-bfb8-01d615b1a942")]
 
 // Version information for an assembly consists of the following four values:
 //
